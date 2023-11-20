4dbb7b41e934b9fd48a790a2a0536b6234f58389
lya <lyasantosdev@gmail.com>
2023-11-20 17:08:11 -0400

    id finViewByld Recuperando o ID com o método findViewByld

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.stackmobile.recuperandooidcomomtodofindviewbyid"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.stackmobile.recuperandooidcomomtodofindviewbyid"
--- app/src/main/java/com/stackmobile/recuperandooidcomomtodofindviewbyid/MainActivity.java
+++ app/src/main/java/com/stackmobile/recuperandooidcomomtodofindviewbyid/MainActivity.java
@@ -1,8 +1,10 @@
 package com.stackmobile.recuperandooidcomomtodofindviewbyid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Button btCadastrar = findViewById(R.id.btCadastrar);
+
+        btCadastrar.setOnClickListener(v -> Toast.makeText(MainActivity.this, "botao foi cliado!", Toast.LENGTH_SHORT).show());
+
+        }
+
+
 }
