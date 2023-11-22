b2b9b869a10b54ff9ae538465d0410869a78db89
lya <lyasantosdev@gmail.com>
2023-11-22 17:38:13 -0400

    Id finViewByld Recuperando o ID com o método findViewByld Têm comentários

--- app/src/main/java/com/stackmobile/recuperandooidcomomtodofindviewbyid/MainActivity.java
+++ app/src/main/java/com/stackmobile/recuperandooidcomomtodofindviewbyid/MainActivity.java
@@ -1,12 +1,19 @@
 package com.stackmobile.recuperandooidcomomtodofindviewbyid;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    //outra forma de criar seria global
+
+    //por exxemplo: private Button btCadastrar;
+    // private TextView txtMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btCadastrar = findViewById(R.id.btCadastrar);
+        TextView txtMensagem = findViewById(R.id.txtMensagem);
 
-        btCadastrar.setOnClickListener(v -> Toast.makeText(MainActivity.this, "botao foi cliado!", Toast.LENGTH_SHORT).show());
+        // btCadastrar.setOnClickListener(v -> Toast.makeText(MainActivity.this, "botao foi clicado!", Toast.LENGTH_SHORT).show());
+        btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtMensagem.setText("O usuário foi cadastrado");
+            }
+        });
 
-        }
-
+    }
 
 }
+
+
+
