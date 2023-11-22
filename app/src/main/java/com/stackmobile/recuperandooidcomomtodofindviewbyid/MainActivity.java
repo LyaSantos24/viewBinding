65599ca33fc0ced749ec52c0cd4eabdb204d7a09
lya <lyasantosdev@gmail.com>
2023-11-22 18:37:08 -0400

    Id viewBinding Recuperando o ID com o método viewBinding Têm comentários no dentro do código

--- app/src/main/java/com/stackmobile/recuperandooidcomomtodofindviewbyid/MainActivity.java
+++ app/src/main/java/com/stackmobile/recuperandooidcomomtodofindviewbyid/MainActivity.java
@@ -3,37 +3,42 @@ package com.stackmobile.recuperandooidcomomtodofindviewbyid;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.stackmobile.recuperandooidcomomtodofindviewbyid.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-
-    //outra forma de criar seria global
-
-    //por exxemplo: private Button btCadastrar;
-    // private TextView txtMensagem;
+    private ActivityMainBinding binding; //se criou uma variavel binding que vai herdar da ActivityMain, obs: Binding é biblioteca
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater()); //metodo inflate vai inflar layout, com esse metodo vou passar layout para ActivityMain
+        //dai se utiliza metodo getlayoutinflater, onde ele vai recuperar o layout, sabendo exatemente qual é o layout dessa activity
+        //Como ele sabe: Ele sabe atraves do método da variavel binding
+        setContentView(binding.getRoot());  //.. dai vamos saber todos as referencias que estão nesse layoutbinding
 
-        Button btCadastrar = findViewById(R.id.btCadastrar);
-        TextView txtMensagem = findViewById(R.id.txtMensagem);
+        //Também poderia guardar dentro da variavel, por exemplo: guardar o botao dentro da variavel
+        //Button btCadastrar = binding.btCadastrar;
+        // dai é so remover o binding embaixo, e ficará btCadastrar.setOn..
+        // tambem poderia criar uma variavel para textMensagem
 
-        // btCadastrar.setOnClickListener(v -> Toast.makeText(MainActivity.this, "botao foi clicado!", Toast.LENGTH_SHORT).show());
-        btCadastrar.setOnClickListener(new View.OnClickListener() {
+        // que ficaria TextView textMensagem = binding.TxtMensagem;
+        // removeria o binding do começo também, deixando ou passando só o txtMensagem.setText()....
+        binding.btCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtMensagem.setText("O usuário foi cadastrado");
+
+                binding.txtMensagem.setText("O usuário foi cadastrado");
+
+
             }
         });
 
+
     }
 
+
 }
-
-
-
